711881abb00abfe493437e9f2ceb396efe07a288
Lucas Santos <lucasalbertols@gmail.com>
2025-06-19 21:50:18 -0300

    Programa concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,12 +4,20 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get;  set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
+        
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
-
+﻿
 // TODO: Realizar os testes com as classes Nokia e Iphone
+using System;
+using DesafioPOO.Models;
+
+namespace SistemaCelulares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone nokia = new Nokia("11999999999", "Nokia 3310", "123456789012345", 32);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Snake");
+
+            Console.WriteLine();
+
+            Smartphone iphone = new Iphone("11988888888", "iPhone 13", "987654321098765", 128);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
